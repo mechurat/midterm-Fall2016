7b2f317a4fe6e82d5a8e3a3232ff495537cca417
Tyler Mechura <tjm471@nyu.edu>
2016-10-12 16:01:44 -0400

    Document Update
    Removed some files, changed "project plan" folder to "style guide,"
    added wireframe images.

--- index.js
+++ index.js
@@ -2,44 +2,43 @@ var express = require('express');
 var hbs = require('express-handlebars');
 
 var app = express();
-var portNum = process.env.PORT || 8080;
 
+var portNum = process.env.PORT || 8080;
 app.set('port', portNum);
 
-app.engine('handlebars', hbs({defaultLayout: 'main'}) );
+app.engine('handlebars', hbs({
+    defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
-//request URL function
-app.use(function(req, res, next){
-    console.log('request to ' + req.url);
-    next();
-});
-
 //Get the /home url and apply handlebars template to it
-app.get('/:name', function(req,res, next){
+app.get('/', function (req, res, next) {
     res.status(200);
-    res.render('home',
-        {
-            company: 'Old Glory Tattoo Co.',
-            address: '',
-            hours: '',
-            phone: '',
-            artists: [
-                {name: '', style: '', days: '' },
+    res.render('home', {
+        company: 'Old Glory Tattoo Co.',
+        address: '',
+        hours: '',
+        phone: '',
+        artists: [
+            {
+                name: '',
+                style: '',
+                days: '',
+                tag: ''
+            },
             ],
-            facebookURL: '',
-            galleryURL: '',
-        }
-    );
-    next();
+        facebookURL: '',
+        galleryURL: '',
+    });
 });
 
 //404 Catch all Error
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
+    console.log(err);
     res.status(404);
     res.send('404 - Not Found');
 });
 
-app.listen(portNum, function(){
+app.listen(portNum, function () {
     console.log('listening on port ', portNum);
 });
